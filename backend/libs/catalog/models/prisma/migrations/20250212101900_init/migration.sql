bf897866dba2e429bfcc81c19e40d9b3d6fc7665
Klimov Andrey <klimov.andrey.n@yandex.ru>
2025-02-12 17:21:13 +0700

    модель для призмы

---
+++ backend/libs/catalog/models/prisma/migrations/20250212101900_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "added_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "image_path" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "article" TEXT NOT NULL,
+    "strings_сount" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "products_added_date_idx" ON "products"("added_date");
+
+-- CreateIndex
+CREATE INDEX "products_type_idx" ON "products"("type");
+
+-- CreateIndex
+CREATE INDEX "products_strings_сount_idx" ON "products"("strings_сount");
+
+-- CreateIndex
+CREATE INDEX "products_price_idx" ON "products"("price");
+
+-- CreateIndex
+CREATE INDEX "products_created_at_idx" ON "products"("created_at");
